f42b52039966771649a2e6a1517c46707aff8137
arealadmin <73879235+arealadmin@users.noreply.github.com>
2024-01-30 18:39:36 -0800

    Update A47HUB.lua

--- A47/A47HUB.lua
+++ A47/A47HUB.lua
@@ -12,16 +12,16 @@ local library = loadstring(game:HttpGet(
     _G.SliderColor = Color3.fromRGB(246, 20, 255)
     _G.DraggerCircleColor = Color3.fromRGB(246, 20, 255)
     local scripts = w:CreateFolder("Script Menu")
-    local player = w:CreateFolder("Player")
+    local player = w:CreateFolder("Toggles")
     local scriptMenu
 --------------------------------------------------------------------
---game:GetService("CoreGui").PurchasePrompt.Enabled = false
+game:GetService("CoreGui").PurchasePrompt.Enabled = false
 --------------------------------------------------------------------
 
 getgenv().playerESP = false
 
 
-scripts:Dropdown("Scripts", {"Dark Dex", "Simple Spy", "Infinite Yield", "V.G Hub", "Aim Bot", "The Survival Game"}, true,
+scripts:Dropdown("Scripts", {"Dark Dex", "Simple Spy", "Infinite Yield", "Aim Bot"}, true,
     function(script) -- true/false, replaces the current title "Dropdown" with the option that t
        scriptMenu = script;
     end)
@@ -33,17 +33,13 @@ scripts:Button("Launch Script", function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/master/SimpleSpy.lua"))()
     elseif scriptMenu == "Infinite Yield" then
         loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'), true))()
-    elseif scriptMenu == "V.G Hub" then
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
     elseif scriptMenu == "Aim Bot" then
         aimBOT()
-    elseif scriptMenu == "The Survival Game" then
-        loadstring(game:HttpGet("https://github.com/joeengo/exploiting/blob/main/tsg.lua?raw=true", true))()
     end
 end)
 
 -- a:DestroyGui()
---[[
+--[
 player:Slider("Walk Speed", {
     min = 16,
     max = 200,
@@ -59,13 +55,19 @@ player:Toggle("Player ESP", function(on)
     end
 end)
 
+player:Toggle("Auto Smith", function(on)
+    smith = on
+    if on == true then
+        autoSmith()
+    end
+end)
+
 player:Button("TP Tool",function()
     for i,v in pairs(game.Players.LocalPlayer:FindFirstChildOfClass("Backpack"):GetChildren()) do
 		if v.Name == "TP Tool" then
 		v:Destroy()
 	end
 end
-
 	mouse = game.Players.LocalPlayer:GetMouse()
 	tool = Instance.new("Tool")
 	tool.RequiresHandle = false
@@ -77,6 +79,25 @@ end
 	end)
 	tool.Parent = game.Players.LocalPlayer.Backpack
 end)
+
+function autoSmith()
+    spawn(function()
+        while smith == true do
+            for i, v in pairs(game.Workspace:GetDescendants()) do
+                if v:IsA("ProximityPrompt") then
+                    if v.Parent:IsA("Part") then
+                        if v.Parent.Size == Vector3.new(1,1,1) and v.Name ~= "ConscriptionProx" then
+                            if (v.Parent.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude < 50 then
+                            fireproximityprompt(v)
+                            end
+                        end
+                    end
+                end
+            end
+            task.wait(0.7)
+        end
+    end)
+end
 
 function toggleESP()
     spawn(function()
