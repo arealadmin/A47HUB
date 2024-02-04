ba1a6a81af3e9cd5e898d737826590279e2d6524
arealadmin <73879235+arealadmin@users.noreply.github.com>
2024-02-03 20:11:26 -0800

    Update A47HUB.lua

--- A47/A47HUB.lua
+++ A47/A47HUB.lua
@@ -75,7 +75,7 @@ function autoProx()
                 if v:IsA("ProximityPrompt") then
                     if v.Parent:IsA("Part") then
                         if v.Parent.Size == Vector3.new(1,1,1) and v.Name ~= "ConscriptionProx" then
-                            if (v.Parent.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude < 30 then
+                            if (v.Parent.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude < 50 then
                             fireproximityprompt(v)
                             end
                         end
