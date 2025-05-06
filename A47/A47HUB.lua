bd716bff3cfc9911e82fabe3bcc3cc67b49778a9
arealadmin <73879235+arealadmin@users.noreply.github.com>
2025-05-05 19:53:40 -0700

    Update A47HUB.lua

--- A47/A47HUB.lua
+++ A47/A47HUB.lua
@@ -304,7 +304,9 @@ end
 ----------------------------------------------------------------------
 
 if game.PlaceId == 104715542330896 then
-    local blockSpin = w:CreateFolder("BlockSpin")
+    local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+    local w = library:CreateWindow("BlockSpin")
+    local blockSpin = w:CreateFolder("Main")
     blockSpin:Button("Proximity Extender", function(on)
         if on then
             local function autoProx()
